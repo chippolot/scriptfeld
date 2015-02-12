a86d447bbdb9d12e8f6012ff65702c53bd9e4d34
Ben Smith <ben@z2.com>
2015-02-11 21:45:49 -0800

    Made much faster w/ cpickle

--- generate-script.py
+++ generate-script.py
@@ -1,49 +1,66 @@
 #!/usr/bin/env python -tt
   
-import pickle
+import cPickle as pickle
 import random
 from settings import *
 from markov import *
 
+weighted_speakers = pickle.load(open('data/_characters.p', 'rb'))
+dialogue_chains = {}
+
 def generate_script():
-	# pick a number of characters to be in the scene
-	num_characters = random.randint(MIN_CHARACTERS, MAX_CHARACTERS)
-	weighted_characters = pickle.load(open('data/_characters.p', 'rb'))
+	while True:
 
-	# pick characters from weighted list
-	cast = []
-	while len(cast) < num_characters:
-		cast_member = random.choice(weighted_characters)
-		if cast_member in cast:
+		# pick a number of characters to be in the scene
+		num_speakers = random.randint(MIN_SPEAKERS, MAX_SPEAKERS)
+
+		# pick characters from weighted list
+		cast = []
+		while len(cast) < num_speakers:
+			cast_member = random.choice(weighted_speakers)
+			if cast_member in cast:
+				continue
+			cast.append(cast_member)
+
+		# load cast dialogue chains
+		for cast_member in cast:
+			if not cast_member in dialogue_chains:
+				dialogue_chains[cast_member] = pickle.load(open('data/'+cast_member+'.p', 'rb'))
+
+		# create script
+		script = ""
+		last_speaker = ""
+		num_utterances = random.randint(MIN_UTTERANCES, MAX_UTTERANCES)
+		for i in range(num_utterances):
+
+			# choose speaker
+			speaker = last_speaker
+			while speaker == last_speaker:
+				speaker = random.choice(cast)
+			last_speaker = speaker
+			script += speaker + "\n"
+
+			# say something
+			sentence = markov(dialogue_chains[speaker])
+
+			script += sentence + "\n"
+
+			# seperator
+			script += "\n"
+
+		# remove the final two line breaks
+		script = script[:-2]
+
+		if len(script) > MAX_CHARACTERS:
 			continue
-		cast.append(cast_member)
 
-	# load cast dialogue chains
-	dialogue_chains = {}
-	for cast_member in cast:
-		dialogue_chains[cast_member] = pickle.load(open('data/'+cast_member+'.p', 'rb'))
-
-	# create script
-	script = []
-	last_speaker = ""
-	num_utterances = random.randint(MIN_UTTERANCES, MAX_UTTERANCES)
-	for i in range(num_utterances):
-
-		# choose speaker
-		speaker = last_speaker
-		while speaker == last_speaker:
-			speaker = random.choice(cast)
-		last_speaker = speaker
-		print speaker
-
-		# say something
-		sentence = markov(dialogue_chains[speaker])
-		print sentence
-
-		# seperator
-		print ""
-
-	print "\n".join(script)
+		return script
 
 if __name__ == '__main__':
-	generate_script()
+	while True:
+		print "---------------------------------------"
+		print generate_script()
+		print "---------------------------------------"
+		choice = raw_input("Another (y/n): ")
+		if choice == "n":
+			break
